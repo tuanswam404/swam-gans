5c3535dcbcb045d037bddbc1f653ec538d64640c
tuanswam404 <43093593+tuanswam404@users.noreply.github.com>
2018-12-27 06:48:21 +0000

    Create kontol.sh

---
+++ kontol.sh
@@ -0,0 +1,684 @@
+#!/system/xbin/bash
+
+green="\033[32;1m"
+
+yellow="\033[33;1m"
+
+indigo="\033[34;1m"
+
+ungu="\033[35;1m"
+
+cyan="\033[36;1m"
+
+white="\033[37;1m"
+
+red="\033[31;1m"
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo $red" TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+
+echo $red" HARAP SABAR MENUNGGU :)" 
+
+sleep 1
+
+echo '''\a
+
+\033[34;1m _
+
+\033[34;1m / |
+
+\033[34;1m | |
+
+\033[34;1m | |
+
+\033[34;1m |_| '''
+
+sleep 1
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo $red" TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+
+echo $red" HARAP SABAR MENUNGGU :)"
+
+sleep 1
+
+echo '''\a
+
+\033[32;1m ____
+
+\033[32;1m |___ \
+
+\033[32;1m __) |
+
+\033[32;1m / __/
+
+\033[32;1m |_____| '''
+
+sleep 1
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo $red" TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+
+echo $red" HARAP SABAR MENUNGGU :)"
+
+sleep 1
+
+echo '''\a
+
+\033[35;1m _____
+
+\033[35;1m |___ /
+
+\033[35;1m |_ \
+
+\033[35;1m ___) |
+
+\033[35;1m |____/ '''
+
+sleep 1
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo $red" TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+
+echo $red" HARAP SABAR MENUNGGU :)"
+
+sleep 1
+
+echo '''\a
+
+\033[33;1m _ _
+
+\033[33;1m | || |
+
+\033[33;1m | || |_
+
+\033[33;1m |__ _|
+
+\033[33;1m |_| '''
+
+sleep 1
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo $red" TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+
+echo $red" HARAP SABAR MENUNGGU :)"
+
+sleep 1
+
+echo '''\a
+
+\033[36;1m ____
+
+\033[36;1m | ___|
+
+\033[36;1m |___ \
+
+\033[36;1m ___) |
+
+\033[36;1m |____/ '''
+
+sleep 1
+
+clear
+
+echo
+
+echo $red
+
+pkg install toilet -y
+
+clear
+
+echo $green
+
+pkg install figlet -y
+
+clear
+
+echo $cyan
+
+pkg install ruby -y
+
+clear
+
+gem install figlet
+
+clear
+
+gem install lolcat
+
+clear
+
+pkg install curl
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+sleep 1
+
+toilet -f future " Install selesai" | lolcat
+
+sleep 5
+
+clear
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+toilet -f future " Wellcome to my tools" | lolcat
+
+sleep 5
+
+clear
+
+sh 77
+
+# Bersihkan Layar
+
+clear
+
+blue='\033[34;1m'
+
+green='\033[32;1m'
+
+purple='\033[35;1m'
+
+cyan='\033[36;1m'
+
+red='\033[31;1m'
+
+white='\033[37;1m'
+
+yellow='\033[33;1m'
+
+###################################################
+
+# CTRL C-
+
+###################################################
+
+trap ctrl_c INT
+
+ctrl_c() {
+
+clear
+
+echo $red"[#]> (Ctrl + C ) Detected, 
+
+Trying To Exit ... "
+
+sleep 1
+
+echo ""
+
+echo $yellow"[#]> Thank You For Using My 
+
+Tools ... "
+
+sleep 1
+
+echo ""
+
+echo $white"[#]> KiLLeR Cyber Team Was 
+
+Here ... "
+
+read enter
+
+exit
+
+}
+
+=================================================
+
+echo $yellow
+
+figlet -f smshadow "Tools Tuan Swam"
+
+echo $indigo
+
+date
+
+echo $purple"___________________________________________________"
+
+echo $cyan" ["$red"+"$cyan"]Author by	: Tuan  Swam "
+
+echo $cyan" ["$red"+"$cyan"]Thanks To: MR.CL4Y"
+
+echo $cyan" ["$red"+"$cyan"]Team : KiLLeR Cyber Team		"
+
+echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/tuanswam404"
+
+echo
+$cyan" ["$red"+"$cyan"]Facebook: Tuan Swam"
+
+echo
+$cyan" ["$red"+"$cyan"]Whatshap: 083867184119"
+
+echo $purple"___________________________________________________"
+
+echo $cyan" 1) install Bokep by Tuan_Swam"
+
+echo $green" 2) install Brute-Webdav-Tuan_Swam"
+
+echo $yellow" 3) install tools installer v1"
+
+echo $purple" 4) install tools installer v2"
+
+echo $red" 5) install tools installer v3"
+
+echo $white" 6) install tools RCT"
+
+echo $blue" 7) install tools TUANB4DUT"
+
+echo $green" 8) install tools Cyb3r_Gh0st"
+
+echo $purple" 9) install DarkFly-Tool"
+
+echo $yellow" 10) install tools MR.TR3V!0N"
+
+echo $blue"   11) biodata Tuan_Swam"
+
+echo $red" 0) Exit "
+
+echo $white""
+
+read -p "[root@Tuan_Swam]> " bos;
+
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+
+then
+
+clear
+
+echo $cyan"install Bokep by Tuan Swam"
+
+sleep 1
+
+Astagfirullah...;v
+
+Otak² Bokep Nih:v $HOME
+
+echo $red" Jangan Coly Mulu Boedjank:v "
+
+fi
+
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+
+then
+
+clear
+
+echo $green"install brute-webdav"
+
+sleep 2
+
+pkg update && pkg upgrade
+
+pkg install git
+
+pkg install sh
+
+pkg install php
+
+git clone https://github.com/RabbitCL4Y/brute-webdav
+
+mv -f brute-webdav $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [$bos = 3 ] || [ $bos = 3 ]
+
+then
+
+clear
+
+echo $yellow"install tools installer v1"
+
+sleep 3
+
+pkg update && pkg upgrade
+
+pkg install git 
+
+pkg install sh
+
+git clone https://github.com/RabbitCL4Y/CL4y
+
+mv -f CL4y $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+
+then
+
+clear
+
+echo $purple"install tools installer v2"
+
+sleep 1
+
+pkg update && pkg upgrade
+
+pkg install git 
+
+pkg install sh
+
+git clone
+
+https://github.com/RabbitCL4Y/CL4y-v2
+
+mv -f CL4y-v2 $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+
+then
+
+clear
+
+echo $red"installing tools installer v3"
+
+sleep 2
+
+pkg update && pkg upgrade
+
+pkg install git 
+
+pkg install sh
+
+git clone
+
+https://github.com/RabbitCL4Y/CL4Yv3
+
+mv -f CL4Yv3 $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 6 ] || [ $bos = 6 ]
+
+then
+
+clear
+
+echo $white"installing tools RCT"
+
+sleep 3
+
+apt update && apt upgrade
+
+pkg install git
+
+git clone https://github.com/RabCT/rct
+
+mv -f rct $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 7 ] || [ $bos = 7 ]
+
+then
+
+clear
+
+echo $blue"installing tools TUANB4DUT"
+
+sleep 1
+
+apt update && apt upgrade
+
+pkg install python python2 vim figlet curl
+
+pkg install php
+
+pip2 install lolcat
+
+pkg install git
+
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+
+mv -f TOOLSINSTALLERv4 $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 8 ] || [ $bos = 8 ]
+
+then
+
+clear
+
+echo "installing tools Cyb3r_Gh0st"
+
+sleep 2
+
+pkg update && pkg upgrade
+
+pkg install figlet
+
+pkg install php
+
+pkg install python python2 vim figlet curl
+
+pip2 install lolcat
+
+pkg install git
+
+git clone
+
+https://github.com/cyberghost69/bima-bagus-jabang-v3
+
+mv -f bima-bagus-jabang-v3 $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 9 ] || [ $bos = 9 ]
+
+then
+
+clear
+
+echo $purple"installing tools DarkFly-Tool"
+
+sleep 3
+
+apt upgrade
+
+apt update
+
+pkg install python2
+
+pkg install git
+
+pkg install php
+
+git clone https://github.com/Ranginang67/DarkFly-Tool
+
+mv -f DarkFly-Tool $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+
+if [ $bos = 10 ] || [ $bos = 10 ]
+
+then
+
+clear
+
+echo $yellow"installing tools MR.TR3V!0N"
+
+sleep 4
+
+apt update && apt upgrade
+
+pkg install figlet
+
+pkg install lolcat
+
+pkg install ruby
+
+pkg install gem
+
+gem install lolcat
+
+pkg install unzip
+
+pkg install python
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/Rusmana-ID/rus
+
+mv -f rus $HOME
+
+echo $red" T E R I N S T A L L "
+
+fi
+
+if [ $bos = 11 ] || [ $bos = 11 ]
+
+then
+
+clear
+
+echo $cyan"biodata Tuan Swam"
+
+sleep 11
+
+Tuan Swam Baik Banget;v
+
+Gans Lagi:v
+
+Masih Bocah Gk tau apa²
+
+Masih Polos...$HOME
+
+echo $red" Tuan Swam Gans "
+
+fi
+
+
+if [ $bos = 0 ] || [ $bos = 0 ]
+
+then
+
+echo " Thanks To KiLLeR Cyber Team "
+
+sleep 1
+
+echo " Tuan Swam Was Here "
+
+sleep 1
+
+echo " Fuck You "
+
+sleep 1
+
+echo " Gw Gans Tq "
+
+sleep 1
+
+echo " Thanks To MR.CL4Y "
+
+sleep 1
+
+echo " Script Kiddie "
+
+sleep 1
+
+exit
+
+fi
